55a8ebe310f0160eeff0c6bdf12e4e639b82dc4b
mariusalt <30729713+mariusaltalt@users.noreply.github.com>
2017-08-10 15:08:44 +0200

    100820171

---
+++ quiz_time/models.py
@@ -0,0 +1,72 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+import csv
+
+author = 'Marius'
+
+doc = """
+Play a loaded quiz
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'quiz_time'
+    players_per_group = None
+    
+
+    with open('quiz_time\quiz.csv') as f:
+    	questions = list(csv.DictReader(f, delimiter=';'))
+
+    num_rounds = len(questions)
+
+
+
+class Subsession(BaseSubsession):
+    def creating_session(self):
+        if self.round_number == 1:
+            self.session.vars['questions'] = Constants.questions
+            ## ALTERNATIVE DESIGN:
+            ## to randomize the order of the questions, you could instead do:
+
+            # import random
+            # randomized_questions = random.sample(Constants.questions, len(Constants.questions))
+            # self.session.vars['questions'] = randomized_questions
+
+            ## and to randomize differently for each participant, you could use
+            ## the random.sample technique, but assign into participant.vars
+            ## instead of session.vars.
+
+        for p in self.get_players():
+            question_data = p.current_question()
+            p.question_id = question_data['id']
+            p.question = question_data['question']
+            p.solution = question_data['solution']
+
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    question_id = models.PositiveIntegerField()
+    question = models.CharField()
+    solution = models.CharField()
+    submitted_answer = models.CharField(widget=widgets.RadioSelect())
+    is_correct = models.BooleanField()
+ #   correct_answers=models.IntegerField(
+  #  	default=0
+   # 	)
+
+    def current_question(self):
+        return self.session.vars['questions'][self.round_number - 1]
+
+    def check_correct(self):
+    	self.is_correct = self.submitted_answer == self.solution
+
+#    def count(self):
+ #   	if self.submitted_answer == self.solution:
+  #  		self.correct_answers=self.correct_answers.in_previous_rounds() + 1
+
--- settings.py
+++ settings.py
@@ -150,6 +150,12 @@ SESSION_CONFIGS = [
      'num_demo_participants': 4,
      'app_sequence': ['fortune_treat'],
      'treatment':1000
+ },
+ {
+     'name': 'quiz_time',
+     'display_name': 'Play Quiz (loaded)',
+     'num_demo_participants': 1,
+     'app_sequence': ['quiz_time']
  }
 
 ]
